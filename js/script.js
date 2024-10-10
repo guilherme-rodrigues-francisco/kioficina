adaedad306e69a2c622bc476500d5b8ca33f9be7
Guilherme Rodrigues <guifrancisco2002@outlook.com>
2024-10-10 14:05:43 -0300

    finish dots

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.banner__slide').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
+        dots: true,
         autoplay: true,
         autoplaySpeed: 2000,
       });
