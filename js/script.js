0dfcaf2cf97090ab32ac4a5ba8e23da294f75c70
guifrancisco2002 <guifrancisco2002@outlook.com>
2024-10-16 23:33:54 -0300

    implementa seção de marcas

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
       });
   });
+
+  $(document).ready(function(){
+    $('.autobrands__slide').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 2000,
+      });
+  });
